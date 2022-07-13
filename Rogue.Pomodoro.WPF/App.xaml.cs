abeb08a97c10695c1d40a8a7a9f3c1eb08cf6e99
Pendramon <pendramon2@gmail.com>
2022-07-13 02:23:28 +0200

    Then the developer said "May there be order among the chaos"

--- Rogue.Pomodoro.WPF/App.xaml.cs
+++ Rogue.Pomodoro.WPF/App.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows;
 namespace Rogue.Pomodoro.WPF;
 
 /// <summary>
-/// Interaction logic for App.xaml
+/// Interaction logic for App.xaml.
 /// </summary>
 public partial class App : Application
 {
@@ -14,18 +14,18 @@ public partial class App : Application
     {
         var services = new ServiceCollection();
         ConfigureServices(services);
-        this.serviceProvider = services.BuildServiceProvider();
+        serviceProvider = services.BuildServiceProvider();
+    }
+
+    protected async override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        var bootstrapper = serviceProvider.GetRequiredService<Bootstrapper>();
+        await bootstrapper.InitializeAsync();
     }
 
     private void ConfigureServices(ServiceCollection services)
     {
         services.AddSingleton<MainWindow>();
-    }
-
-    protected override async void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
-        var bootstrapper = serviceProvider.GetRequiredService<Bootstrapper>();
-        await bootstrapper.InitializeAsync();
     }
 }
